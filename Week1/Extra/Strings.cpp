d6b53e1f4afdb636720390e0767c2b462f44ee42
Chatura Ahangama <chatura@alphacodelab.com>
2016-06-01 11:31:35 -0700

    The Strings
    New Strings program

---
+++ Week1/Extra/Strings.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string> //Inlcude the header just to be safe
+using namespace std;
+
+
+int main()
+{
+    //Syntax datatype arrayName [[numberOfCells]];
+    string names[4];
+    
+    
+    cin >> names[0];
+    cin >> names[1];
+    cin >> names[2];
+    cin >> names[3];
+    
+    cout<< names[0]
+        << names[1]
+        << names[2]
+        << names[3];
+    
+
+
+}
+
+
